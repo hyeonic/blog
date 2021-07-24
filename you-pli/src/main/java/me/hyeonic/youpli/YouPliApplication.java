72d7292d6e576f8e873e74b1988303fb22ef839f
hyeonic <evan3566@gmail.com>
2021-07-24 13:29:59 +0900

    feat: you-pli 프로젝트 생성

---
+++ you-pli/src/main/java/me/hyeonic/youpli/YouPliApplication.java
@@ -0,0 +1,13 @@
+package me.hyeonic.youpli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouPliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouPliApplication.class, args);
+    }
+
+}
---
+++ you-pli/src/test/java/me/hyeonic/youpli/YouPliApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hyeonic.youpli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouPliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
