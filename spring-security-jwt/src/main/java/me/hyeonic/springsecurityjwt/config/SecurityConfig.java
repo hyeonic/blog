e8339d633c7d0aa4c505f4a1b9190e03f6ddae6e
hyeonic <evan3566@gmail.com>
2021-07-26 20:42:58 +0900

    feat: JWT 설정 추가 및 Security 설정 추가
    JWT 관련 코드 추가
    Security 설정 추가

--- spring-security-jwt/src/main/java/me/hyeonic/springsecurityjwt/config/SecurityConfig.java
+++ spring-security-jwt/src/main/java/me/hyeonic/springsecurityjwt/config/SecurityConfig.java
@@ -1,11 +1,33 @@
 package me.hyeonic.springsecurityjwt.config;
 
+import lombok.RequiredArgsConstructor;
+import me.hyeonic.springsecurityjwt.jwt.JwtAccessDeniedHandler;
+import me.hyeonic.springsecurityjwt.jwt.JwtAuthenticationEntryPoint;
+import me.hyeonic.springsecurityjwt.jwt.JwtSecurityConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity // 웹 보안 활성화
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtSecurityConfig jwtSecurityConfig;
+
+    // password encoder
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     public void configure(WebSecurity web) {
@@ -15,6 +37,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/h2-console/**",
                         "/favicon.ico"
                 );
+    }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                // token을 사용하는 방식이기 때문에 csrf를 disable
+                .csrf().disable()
+
+                // Exception을 핸들링할 때 직접 만든 클래스를 추가
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                // enable h2-console
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests() // HttpServletRequest를 사용하는 요청들에 대한 접근제한을 설정
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers("/api/signup").permitAll()
+
+                .anyRequest().authenticated() // 나머지는 인증 필요
+
+                .and()
+                .apply(jwtSecurityConfig);
     }
 }
