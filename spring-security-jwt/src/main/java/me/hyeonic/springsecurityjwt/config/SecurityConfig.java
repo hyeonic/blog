714e688c41fbe40201676ac94dfe02a752cc7fca
hyeonic <evan3566@gmail.com>
2021-07-26 20:23:31 +0900

    feat: h2-console 접근 허용 및 초기 데이터 확인

---
+++ spring-security-jwt/src/main/java/me/hyeonic/springsecurityjwt/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package me.hyeonic.springsecurityjwt.config;
+
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity // 웹 보안 활성화
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .antMatchers(
+                        "/h2-console/**",
+                        "/favicon.ico"
+                );
+
+    }
+}
--- spring-security-jwt/src/main/resources/data.sql
+++ spring-security-jwt/src/main/resources/data.sql
@@ -1,2 +1,3 @@
 INSERT INTO USER (USER_ID, EMAIL, PASSWORD, NAME, ROLE)
-VALUES (1, 'admin@email.com', 'admin', 'admin', 'ROLE_ADMIN');
+VALUES (1, 'admin@email.com', 'admin', 'admin', 'ROLE_ADMIN'),
+       (2, 'user@email.com', 'user', 'user', 'ROLE_USER');
