74bfc67b5dc8e0e21271998f5b3483e34b3cad96
hyeonic <evan3566@gmail.com>
2021-08-20 18:55:34 +0900

    fix: 불필요한 import 제거

--- spring-openfeign/src/main/java/me/hyeonic/springopenfeign/controller/api/UserApiController.java
+++ spring-openfeign/src/main/java/me/hyeonic/springopenfeign/controller/api/UserApiController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import me.hyeonic.springopenfeign.controller.dto.api.UserSaveRequestDto;
 import me.hyeonic.springopenfeign.domain.user.User;
 import me.hyeonic.springopenfeign.domain.user.UserRepository;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
