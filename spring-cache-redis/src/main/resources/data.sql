9f32ea21d0d7d7b39f7831c57575d163cddb266b
hyeonic <dev.hyeonic@gmail.com>
2021-08-31 16:26:33 +0900

    feat: 설정 추가 및 data.sql 추가

---
+++ spring-cache-redis/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO USER (EMAIL, NAME, CLICK)
+VALUES ('user1@email.com', 'user1', 0),
+       ('user2@email.com', 'user2', 0),
+       ('user3@email.com', 'user3', 0),
+       ('user4@email.com', 'user4', 0),
+       ('user5@email.com', 'user5', 0);
