d1e3df75966c4210ab49fce93618deb0dc2bec0b
hyeonic <dev.hyeonic@gmail.com>
2021-08-31 14:16:01 +0900

    feat: 프로젝트 초기 설정

---
+++ spring-cache-redis/src/test/java/me/hyeonic/springcacheredis/SpringCacheRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hyeonic.springcacheredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCacheRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
