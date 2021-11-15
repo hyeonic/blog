5d193d81be50afa07e7ba055df233542f6f48077
hyeonic <dev.hyeonic@gmail.com>
2021-11-16 02:24:45 +0900

    feat: Add Start in chapter02

---
+++ java-oop-principle-understanding/src/main/java/me/hyeonic/javaoopprincipleunderstanding/chapter02/Start.java
@@ -0,0 +1,8 @@
+package me.hyeonic.javaoopprincipleunderstanding.chapter02;
+
+public class Start {
+
+    public static void main(String[] args) {
+        System.out.println("Hello OOP!");
+    }
+}
---
+++ java-oop-principle-understanding/src/main/java/me/hyeonic/javaoopprincipleunderstanding/chapter02/Start2.java
@@ -0,0 +1,10 @@
+package me.hyeonic.javaoopprincipleunderstanding.chapter02;
+
+public class Start2 {
+    public static void main(String[] args) {
+        int i;
+        i = 10;
+
+        double d = 20.0;
+    }
+}
