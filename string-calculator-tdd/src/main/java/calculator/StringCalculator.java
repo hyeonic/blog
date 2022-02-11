fde65d6e522c78129fb69c0311ae23d5c4f82105
hyeonic <dev.hyeonic@gmail.com>
2022-02-11 16:21:19 +0900

    feat: 빈 문자열을 입력할 경우 0을 반환하는 기능 추가

---
+++ string-calculator-tdd/src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class StringCalculator {
+    public static int splitAndSum(String text) {
+        if (text.isEmpty()) {
+            return 0;
+        }
+
+        return 1;
+    }
+}
---
+++ string-calculator-tdd/src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,23 @@
+package calculator;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("NonAsciiCharacters")
+class StringCalculatorTest {
+
+    @DisplayName("빈 문자열을 입력할 경우 0을 반환해야 한다.")
+    @Test
+    void splitAndSum_빈문자열() {
+        // given
+        String text = "";
+
+        // when
+        int result = StringCalculator.splitAndSum(text);
+
+        // then
+        assertThat(result).isEqualTo(0);
+    }
+}
