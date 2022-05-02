921c6934da8b742eca3a25366da98b7ece28dc23
hyeonic <dev.hyeonic@gmail.com>
2022-05-02 16:47:43 +0900

    feat: init request-body in blog-code

---
+++ request-body/src/main/java/me/hyeonic/requestbody/RequestBodyApplication.java
@@ -0,0 +1,13 @@
+package me.hyeonic.requestbody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestBodyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestBodyApplication.class, args);
+    }
+
+}
---
+++ request-body/src/test/java/me/hyeonic/requestbody/RequestBodyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hyeonic.requestbody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestBodyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
