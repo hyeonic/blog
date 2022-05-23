b7bc19cd860a1f2c121dcb98bfd12cf14480ee72
hyeonic <dev.hyeonic@gmail.com>
2022-05-23 15:19:10 +0900

    feat: init subway-map project in blog-code

---
+++ subway-map/src/main/java/me/hyeonic/subwaymap/SubwayMapApplication.java
@@ -0,0 +1,13 @@
+package me.hyeonic.subwaymap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubwayMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubwayMapApplication.class, args);
+    }
+
+}
---
+++ subway-map/src/test/java/me/hyeonic/subwaymap/SubwayMapApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hyeonic.subwaymap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubwayMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
