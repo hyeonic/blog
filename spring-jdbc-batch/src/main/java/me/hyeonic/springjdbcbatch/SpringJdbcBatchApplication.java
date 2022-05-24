adf391536c8e19b89f95d99fa3c603ae30b69286
hyeonic <dev.hyeonic@gmail.com>
2022-05-24 20:47:57 +0900

    feat: init spring-jdbc-batch project in blog-code

---
+++ spring-jdbc-batch/src/main/java/me/hyeonic/springjdbcbatch/SpringJdbcBatchApplication.java
@@ -0,0 +1,13 @@
+package me.hyeonic.springjdbcbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJdbcBatchApplication.class, args);
+    }
+
+}
