9349395121665de38fe7464864f47d2d927a7932
hyeonic <dev.hyeonic@gmail.com>
2022-05-24 20:49:39 +0900

    feat: Station 도메인 추가

---
+++ spring-jdbc-batch/src/main/java/me/hyeonic/springjdbcbatch/domain/Station.java
@@ -0,0 +1,20 @@
+package me.hyeonic.springjdbcbatch.domain;
+
+public class Station {
+
+    private final Long id;
+    private final String name;
+
+    public Station(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
