824cdbab567405c43234ef23031a41c0feae4ebc
hyeonic <dev.hyeonic@gmail.com>
2022-07-03 14:24:27 +0900

    feat: Init local-date-time project

---
+++ local-date-time/src/test/java/me/hyeonic/localdatetime/LocalDateTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hyeonic.localdatetime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalDateTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
